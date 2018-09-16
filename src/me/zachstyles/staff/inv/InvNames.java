b383fb49989d6339917daf8148f302197f61b852
ZachPlayzFTW <31602472+iTzZachstyles@users.noreply.github.com>
2018-09-16 18:26:33 -0500

    Create InvNames.java

---
+++ src/me/zachstyles/staff/inv/InvNames.java
@@ -0,0 +1,56 @@
+package me.zachstyles.staff.inv;
+
+import org.bukkit.*;
+
+public class InvNames
+{
+    public static String invname;
+    public static String secInv;
+    public static String bInv;
+    public static String vInv;
+    public static String timeset;
+    public static String whitelist;
+    public static String weather;
+    public static String effects;
+    public static String tools;
+    public static String jumpboost;
+    public static String speed;
+    public static String mobs;
+    public static String extramobs;
+    public static String difficulty;
+    public static String strength;
+    public static String servermanager;
+    public static String adminitems;
+    public static String mobmanager;
+    public static String flymenu;
+    public static String troll;
+    public static String vanishmenu;
+    public static String feedandhealmenu;
+    
+    static {
+        InvNames.invname = String.valueOf(ChatColor.AQUA) + ChatColor.BOLD + ChatColor.UNDERLINE + "ULTIMATESTAFFMODE-GUI";
+        InvNames.secInv = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Gamemodes Menu";
+        InvNames.bInv = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Health & Food";
+        InvNames.timeset = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Set Time Menu";
+        InvNames.whitelist = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Whitelist Menu";
+        InvNames.weather = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Weather";
+        InvNames.effects = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Effects Menu";
+        InvNames.tools = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Tools Menu";
+        InvNames.jumpboost = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Jump Boost Menu";
+        InvNames.speed = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Speed Effect Menu";
+        InvNames.mobs = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Mobs Menu";
+        InvNames.extramobs = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Extra Mobs Menu";
+        InvNames.difficulty = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Difficulty Menu";
+        InvNames.strength = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Strength Effect Menu";
+        InvNames.servermanager = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Server Manager Menu";
+        InvNames.troll = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Troll Menu";
+        InvNames.adminitems = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Admin Items Menu";
+        InvNames.mobmanager = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Mob Manager Menu";
+        InvNames.flymenu = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Fly Menu";
+        InvNames.vanishmenu = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Vanish Menu";
+        InvNames.feedandhealmenu = String.valueOf(ChatColor.GOLD) + ChatColor.BOLD + "Feed & Heal Menu";
+
+
+        InvNames.vInv = ChatColor.BOLD + "StaffModus";
+    }
+}
