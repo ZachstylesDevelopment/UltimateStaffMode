090d185794d55a4b456d17939072cd83c4a12233
Refrac <31602472+RefracDevelopment@users.noreply.github.com>
2019-10-21 19:08:00 -0500

    Update Main.java

--- src/java/me/refrac/staff/Main.java
+++ src/java/me/refrac/staff/Main.java
@@ -1,59 +1,89 @@
 package me.refrac.staff;
 
-import me.refrac.staff.maincommand.MainCmd;
-import me.refrac.staff.menu.Event;
-import me.refrac.staff.util.*;
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Field;
+import me.refrac.staff.commands.*;
+import me.refrac.staff.menu.*;
+import me.refrac.staff.utils.*;
 
 public class Main extends JavaPlugin {
-	public static Main plugin;
 
-	public void onEnable() {
-		Main.plugin = this;
-		Bukkit.getPluginManager().registerEvents(new MainCmd(), this);
-		PluginDescriptionFile VarUtilType = this.getDescription();
-		this.getLogger().info("ULTIMATESTAFFMODE V" + VarUtilType.getVersion() + " starting...");
-		this.getLogger().info("ULTIMATESTAFFMODE V" + VarUtilType.getVersion() + " loading commands...");
-		this.getCommand("staffmode").setExecutor(new MainCmd());
-		this.getLogger().info("ULTIMATESTAFFMODE V" + VarUtilType.getVersion() + " loading your configuration...");
-		this.saveDefaultConfig();
-		this.reloadConfig();
-		this.register();
-		this.setEnabled(true);
-		getLogger().info("ULTIMATESTAFFMODE V" + VarUtilType.getVersion() + " started!");
-		this.getLogger().info("ULTIMATESTAFFMODE V" + VarUtilType.getVersion() + " checking for updates...");
-				getServer().getConsoleSender().sendMessage("------------------------");
-				getServer().getConsoleSender().sendMessage("Your version: " + Main.plugin.getDescription().getVersion());
-				getServer().getConsoleSender().sendMessage("------------------------");
-	}
-			{
-	}
+    public static Main plugin;
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static Main getPlugin() {
-		return (Main) getPlugin((Class) Main.class);
-	}
+    public void onEnable()
+    {
+        // Plugin startup logic
+        UpdateChecker checker;
 
-	public static Plugin getPlugin2() {
-		return Main.plugin;
-	}
+        Logger.log(Logger.LogLevel.OUTLINE,  "********************");
+        Logger.log(Logger.LogLevel.INFO, "Initializing UltimateStaffMode Version: " + Settings.VERSION);
+        Logger.log(Logger.LogLevel.INFO, "Created by: " + Settings.DEVELOPER_NAME);
+        Logger.log(Logger.LogLevel.INFO, "Spigot Link: " + Settings.PLUGIN_URL);
+        Logger.log(Logger.LogLevel.INFO, "Support Link: " + Settings.SUPPORT_DISCORD_URL);
+        Logger.log(Logger.LogLevel.OUTLINE,  "********************");
+        plugin = this;
 
-	public void register() {
-		this.getCommand("staffmode").setExecutor((CommandExecutor) new MainCmd());
-		this.getServer().getPluginManager().registerEvents((Listener) new Event(), (Plugin) this);
-	}
+        Logger.log(Logger.LogLevel.INFO, "Plugin Loading...");
+        Logger.log(Logger.LogLevel.INFO, "Registering Managers...");
+        register();
+        MetricsLite metrics = new MetricsLite (this);
+        Logger.log(Logger.LogLevel.INFO, "Managers Registered!");
 
-	public static void registerEvents(final Plugin plugin, final Listener... listeners) {
-		for (final Listener listener : listeners) {
-			Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
-		}
-	}
+        Logger.log(Logger.LogLevel.INFO, "Registering Commands...");
+        registerCommands();
+        Logger.log(Logger.LogLevel.INFO, "Commands Registered!");
+
+        Logger.log(Logger.LogLevel.INFO, "Loading Config's...");
+        saveDefaultConfig();
+        Logger.log(Logger.LogLevel.INFO, "Config's Registered!");
+        Logger.log(Logger.LogLevel.SUCCESS, "UltimateStaffMode Version: " + Settings.VERSION + " Loaded.");
+        Logger.log(Logger.LogLevel.OUTLINE,  "********************");
+
+        Logger.log(Logger.LogLevel.INFO, "UltimateStaffMode Version: " + Settings.VERSION + " checking for updates...");
+        checker = new UpdateChecker(plugin);
+        if (checker.isConnected())
+        {
+            if (checker.hasUpdate())
+            {
+                Logger.log(Logger.LogLevel.OUTLINE, "********************");
+                Logger.log(Logger.LogLevel.INFO, "UltimateStaffMode is outdated!");
+                Logger.log(Logger.LogLevel.INFO, "Newest version: " + checker.getLatestVersion());
+                Logger.log(Logger.LogLevel.INFO, "Your version: " + plugin.getDescription ().getVersion ());
+                Logger.log(Logger.LogLevel.INFO, "Download: " + Settings.PLUGIN_URL);
+                Logger.log(Logger.LogLevel.OUTLINE, "********************");
+            }
+        }
+    }
+
+    public void onDisable() {
+        // Plugin shutdown logic
+        plugin = null;
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static Main getPlugin() {
+        return (Main) getPlugin((Class) Main.class);
+    }
+
+    public static Plugin getPlugin2() {
+        return plugin;
+    }
+
+    public void registerCommands() {
+        this.getCommand("staffmode").setExecutor(new MainCommand());
+    }
+
+    public void register() {
+        this.getServer().getPluginManager().registerEvents(new Event(), this);
+        this.getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+    }
+
+    public static void registerEvents(final Plugin plugin, final Listener... listeners) {
+        for (final Listener listener : listeners) {
+            Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
+        }
+    }
 }
