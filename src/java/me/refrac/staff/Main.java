e3f5dc0a4368963f9c7a0a4a691280273abc6993
Refrac <31602472+Refrac@users.noreply.github.com>
2020-04-03 10:15:31 -0500

    Update Main.java

--- src/java/me/refrac/staff/Main.java
+++ src/java/me/refrac/staff/Main.java
@@ -16,46 +16,12 @@ public class Main extends JavaPlugin {
     public void onEnable()
     {
         // Plugin startup logic
-        UpdateChecker checker;
-
-        Logger.log(Logger.LogLevel.OUTLINE,  "********************");
-        Logger.log(Logger.LogLevel.INFO, "Initializing UltimateStaffMode Version: " + Settings.VERSION);
-        Logger.log(Logger.LogLevel.INFO, "Created by: " + Settings.DEVELOPER_NAME);
-        Logger.log(Logger.LogLevel.INFO, "Spigot Link: " + Settings.PLUGIN_URL);
-        Logger.log(Logger.LogLevel.INFO, "Support Link: " + Settings.SUPPORT_DISCORD_URL);
-        Logger.log(Logger.LogLevel.OUTLINE,  "********************");
         plugin = this;
 
-        Logger.log(Logger.LogLevel.INFO, "Plugin Loading...");
-        Logger.log(Logger.LogLevel.INFO, "Registering Managers...");
         register();
         MetricsLite metrics = new MetricsLite (this);
-        Logger.log(Logger.LogLevel.INFO, "Managers Registered!");
-
-        Logger.log(Logger.LogLevel.INFO, "Registering Commands...");
         registerCommands();
-        Logger.log(Logger.LogLevel.INFO, "Commands Registered!");
-
-        Logger.log(Logger.LogLevel.INFO, "Loading Config's...");
         saveDefaultConfig();
-        Logger.log(Logger.LogLevel.INFO, "Config's Registered!");
-        Logger.log(Logger.LogLevel.SUCCESS, "UltimateStaffMode Version: " + Settings.VERSION + " Loaded.");
-        Logger.log(Logger.LogLevel.OUTLINE,  "********************");
-
-        Logger.log(Logger.LogLevel.INFO, "UltimateStaffMode Version: " + Settings.VERSION + " checking for updates...");
-        checker = new UpdateChecker(plugin);
-        if (checker.isConnected())
-        {
-            if (checker.hasUpdate())
-            {
-                Logger.log(Logger.LogLevel.OUTLINE, "********************");
-                Logger.log(Logger.LogLevel.INFO, "UltimateStaffMode is outdated!");
-                Logger.log(Logger.LogLevel.INFO, "Newest version: " + checker.getLatestVersion());
-                Logger.log(Logger.LogLevel.INFO, "Your version: " + plugin.getDescription ().getVersion ());
-                Logger.log(Logger.LogLevel.INFO, "Download: " + Settings.PLUGIN_URL);
-                Logger.log(Logger.LogLevel.OUTLINE, "********************");
-            }
-        }
     }
 
     public void onDisable() {
